5de48b7f5069ce034645383242760d1639368f43
pendulm <lonependulm@gmail.com>
2011-08-19 08:51:39 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Circletalk::Application.config.secret_token = '7bfc33d5981f7ef0e83957a14b5ca780a471353940dd2fa0e8f0baf20bdc6ed60030d8897a81f17627049bc4283c66b9513b37b01a5e78c5edd88639868024e9'
